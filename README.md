7c39795fcdc5d85add54444b56a577ffc4390eb6
root <jeff@1.local>
2016-01-16 09:50:01 -0800

    Updating for ease

--- README.md
+++ README.md
@@ -2,24 +2,18 @@
 Installation script to run on iPhone to install RetroArch on iOS 9.
 
 
-
+# Run
 Script must be run ON the iPhone. Must have installed from Cydia:
  - ipainstaller - For installing the Retroarch IPA
  - ldid - the first letter is a lower case L, for codesigning the dylibs
- - unzip - Unzipping my .zip file on the phone
- - wget - for retrieving files
  - git - only used to check out repository. You can SCP the script instead.
 
 To run on your phone, install git through cydia, then open a terminal on your phone:
 - git clone https://github.com/falconscript/RetroArch-ios9.git
-- chmod 777 ./RetroArch-ios9/install-ra-ios9.sh
 - ./RetroArch-ios9/install-ra-ios9.sh
 
 
-If you have AT&T internet, the wgets may fail due to them blocking my host sometimes... File an issue if that happens
-
-
-Once installed:
+# After Installing
  1. Tap Retroarch icon
  2. Tap Online Updater and tap to update EACH of the list EXCEPT for 'Core Updater' (give maybe 10 seconds between each line)
  3. Ready to play! Go to Main Menu -> Load Content -> Navigate in the file menu to choose your ROMs
--- install-ra-ios9.sh
+++ install-ra-ios9.sh
@@ -1,31 +1,17 @@
-# HOST is where the necessary files are stored
-HOST=http://cfalcon.webege.com
-
-
-wget --no-check-certificate "$HOST/ios.zip" # A zip of necessary dylib files
-wget --no-check-certificate "$HOST/retroarch.ipa" # Has been ldid signed already
-
-
-echo "[+] Unzipping..."
-sleep 1
-unzip ios.zip
-
 
 # Sign all dylibs
 echo -e "\n\n[+] Codesigning dylibs..."
 sleep 1
 # ldid sign all dylibs... will have 2 error messages for . and .. which is ok
-for i in `ls -a ./ios | sed s#\*##g`; do ldid -S "./ios/$i"; done 
+for i in `ls -a ./dylibs | sed s#\*##g`; do ldid -S "./dylibs/$i"; done 
 
 # Install RetroArch emulator
-echo "[+] Installing retroarch from dpkg..."
-ipainstaller retroarch.ipa
-echo "[?] Were there any errors? Hit CTRL+C to cancel or enter to continue..."
-read a
+echo "[+] Installing retroarch through IPAInstaller..."
+ipainstaller retroarch.ipa || { echo "[!] WARNING: Failed to install IPA. Possibly retry running the script"; exit 1; }
 
 
 echo "[+] Transferring dylibs to retroarch folder..."
-cp -rn ios/* cp -rn ios/* /private/var/mobile/Containers/Data/Application/*/Documents/RetroArch/cores
+cp -rn dylibs/* /private/var/mobile/Containers/Data/Application/*/Documents/RetroArch/cores
 
 echo "[*] DONE! Try opening retroarch now:"
 echo "  1. Tap Retroarch icon"
